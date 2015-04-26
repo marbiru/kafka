2176704e270ebf45e2dcba3244604ee0bbf7b672
marbiru <uribram@gmail.com>
2015-04-26 17:52:25 +0300

    Initialise main.js
    with an output that prints when the page is loaded

---
+++ main.js
@@ -0,0 +1,10 @@
+// Written in jquery
+
+// Makes the "Refresh" button print a new aphorism
+
+// Prints an aphorism as soon as the page is started
+
+$(function(){
+  $("#output").text("Testing testing");
+
+});
