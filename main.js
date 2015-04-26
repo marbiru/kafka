f2038055c5f1dfe177bf03eb783f473fbad66aa0
marbiru <ubram@Uris-MacBook-Air.local>
2015-04-26 23:48:39 +0300

    Aphorisms load from separate Aphorisms file
    fantastic, not sure how that happened

--- main.js
+++ main.js
@@ -1,15 +1,5 @@
 // Written in jquery
 
-// list of aphorisms
-
-var aphorisms = [
-"Sin always comes in the open, and available to the senses. It walks on its roots, and need not be torn from the ground.",
-"There can be knowledge of evil without believing in it, because there is no more evil than already exists.",
-"Only here is suffering truly suffering. Not because those who suffer in this world are to receive anything in the next, but because what in this world is Suffering in another world is (unchanged and only freed from its opposite) bliss.",
-"The joys of this life are not its own, but our fear of rising to a higher life; the suffering of this life is not its own, but our self-affliction because of that fear.",
-"Death hangs before of us, much as the schoolroom wall held a painting of Alexander the Great at war. What we must do, by our actions, is to darken (or even efface) the picture in our lifetimes.",
-];
-
 // Prints an aphorism as soon as the page is started
 
 $(function(){
