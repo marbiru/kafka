4595adc430f6c2074ec665c8189e9e1f5d77f9f0
marbiru <uribram@gmail.com>
2015-04-26 17:35:00 +0300

    Initialising aphorisms.js
    this is where all the Kafka aphorisms are stored. By making it a standalone file I'm hoping to make things cleaner in my coding: the main "logic" of the main .js file doesn't depend at all on what specific quotes are being used, so it's cleaner to keep the quotes separate

---
+++ aphorisms.js
@@ -0,0 +1,7 @@
+var aphorisms = [
+"Sin always comes in the open, and available to the senses. It walks on its roots, and need not be torn from the ground.",
+"There can be knowledge of evil without believing in it, because there is no more evil than exists anyway.",
+"Only here is suffering suffering. Not because those who suffer in this world are to receive anything in the next, but because what in this world is Suffering in another world (unchanged and only freed from its opposite) is bliss.",
+"The joys of this life are not its own, but our fear of rising to a higher life; the suffering of this life is not its own, but our self-affliction because of that fear.",
+];
+
