3f982bcb8d0a80914c9918c3172f200352545e55
marbiru <ubram@Uris-MacBook-Air.local>
2015-04-26 19:10:02 +0300

    Various cosmetic changes
    Making things look better

--- main.js
+++ main.js
@@ -1,17 +1,6 @@
 // Written in jquery
 
-// Makes the "Refresh" button print a new aphorism
-
-// Prints an aphorism as soon as the page is started
-
-$(function(){
-  $("#output").text("Testing testing");
-
-  // Prints new aphorism when Refresh button is clicked
-
-  $("#refresh_button").click(function(){
-    $("#output").text(print_aphorism());
-  });
+// list of aphorisms
 
 var aphorisms = [
 "Sin always comes in the open, and available to the senses. It walks on its roots, and need not be torn from the ground.",
@@ -19,6 +8,17 @@ var aphorisms = [
 "Only here is suffering suffering. Not because those who suffer in this world are to receive anything in the next, but because what in this world is Suffering in another world (unchanged and only freed from its opposite) is bliss.",
 "The joys of this life are not its own, but our fear of rising to a higher life; the suffering of this life is not its own, but our self-affliction because of that fear.",
 ];
+
+// Prints an aphorism as soon as the page is started
+
+$(function(){
+  $("#output").text(print_aphorism());
+
+  // Prints new aphorism when Refresh button is clicked
+
+  $("#refresh_button").click(function(){
+    $("#output").text(print_aphorism());
+  });
 
 function print_aphorism() {
 	document.getElementById("output").innerHTML = aphorisms[Math.floor(Math.random() * aphorisms.length)]
