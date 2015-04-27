ef823d9a1c99f0ffee723eccd9f357587e9333ab
marbiru <ubram@Uris-MacBook-Air.local>
2015-04-27 12:00:24 +0300

    Footer/FAQ
    basically updating/implementing the footers and faq's

--- aphorisms.js
+++ aphorisms.js
@@ -1,7 +1,7 @@
 var aphorisms = [
 "Sin always comes in the open, and available to the senses. It walks on its roots, and need not be torn from the ground.",
 "There can be knowledge of evil without believing in it, because there is no more evil than already exists.",
-"Only here is suffering suffering. Not because those who suffer in this world are to receive anything in the next, but because Suffering (unchanged and only freed from its opposite) is bliss.",
+"Only here is suffering suffering. Not because those who suffer in this world are to receive anything in the next, but because there Suffering (unchanged and only freed from its opposite) will be bliss.",
 "The joys of this life are not its own, but our fear of rising to a higher life; the suffering of this life is not its own, but our self-affliction because of that fear.",
 "Death hangs before of us, much like a painting of Alexander the Great at war on the schoolroom wall. What we must do, by our actions, is to darken (or even efface) the picture in our lifetimes.",
 ];
