8e0b5df484dcb9e44ef1f2edebf8e0108b308656
marbiru <ubram@Uris-MacBook-Air.local>
2015-04-27 14:58:06 +0300

    Insert one image using js
    but not yet scrolling through full set of images as supposed to

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
-// Written in jquery
-
 // Prints an aphorism as soon as the page is started
 
 $(function(){
   $("#refresh_button").click(function(){
     $("#output").text(print_aphorism());
   });
+  // prints an aphorism on mouse-click
   $( "#refresh_button" ).trigger( "click" );
-
 
 function print_aphorism() {
 	document.getElementById("output").innerHTML = aphorisms[Math.floor(Math.random() * aphorisms.length)]
     };
 
 });
+
+// print a photo to the pol_image field
+
+var photo_ex = '<img src="http://i.imgur.com/cmWb2x8.jpg" alt="David Cameron" height="349" width="366">'
+var photo_array = [
+'<img src="http://i.imgur.com/cmWb2x8.jpg" alt="David Cameron" height="349" width="366">',
+'<img src="http://i.imgur.com/15se11u.jpg" alt="Ed Miliband" height="349" width="366">',
+];
+
+$('#pol_image').prepend(photo_array[1])
 
 // on click, the 'photo credits' button slides out the details of any photo credits
 
@@ -30,4 +38,3 @@ $(function(){
 	});
 	$( "#about_button" ).trigger( "click" );
 });
-
