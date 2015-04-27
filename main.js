420eed063290b1f909bf907eafa6780a9c1473d0
marbiru <ubram@Uris-MacBook-Air.local>
2015-04-27 11:03:07 +0300

    Using trigger click on refresh button
    "a better way to structure this is to do the refresh button binding
    first, then trigger a click event. That way if you want to change what
    the button does you will only have to change it in one place"

--- main.js
+++ main.js
@@ -3,13 +3,11 @@
 // Prints an aphorism as soon as the page is started
 
 $(function(){
-  $("#output").text(print_aphorism());
-
-  // Prints new aphorism when Refresh button is clicked
-
   $("#refresh_button").click(function(){
     $("#output").text(print_aphorism());
   });
+  $( "#refresh_button" ).trigger( "click" );
+
 
 function print_aphorism() {
 	document.getElementById("output").innerHTML = aphorisms[Math.floor(Math.random() * aphorisms.length)]
