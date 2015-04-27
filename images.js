a81b29bf974dcf97ec6d45bba462623a3da31ffb
marbiru <code@uribram.com>
2015-04-27 18:20:35 +0300

    Lots of tweaks

--- images.js
+++ images.js
@@ -3,6 +3,8 @@ var counter_for_photos = 0
 var photo_array = [
 <img src="http://i.imgur.com/cmWb2x8.jpg" alt="David Cameron" height="349" width="366">,
 <img src="http://i.imgur.com/15se11u.jpg" alt="Ed Miliband" height="349" width="366">,
+<img src="http://i.imgur.com/S3DMtjm.jpg" alt="David Cameron">,
+<img src="http://i.imgur.com/kph89Wo.jpg" alt="Ed Miliband">,
 ];
 
 // changes the photo of the politician on 
