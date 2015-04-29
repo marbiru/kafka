e9eb9989180e1014e2df2fb0be2d5835439b64ba
marbiru <code@uribram.com>
2015-04-29 08:04:36 +0300

    Largely broken photo-picker
    currently: number incrementer finally works. Prepend is a problem
    (needs some kind of "replace" instead).

--- main.js
+++ main.js
@@ -18,11 +18,37 @@ function print_aphorism() {
 var photo_counter = 1;
 
 var photo_array = [
-		'<img src="http://i.imgur.com/cmWb2x8.jpg" alt="David Cameron" height="349" width="366">',
-		'<img src="http://i.imgur.com/15se11u.jpg" alt="Ed Miliband" height="349" width="366">',
+		'<img src="cameron.jpg" alt="David Cameron" height="349" width="366">',
+		'<img src="miliband.jpg" alt="Ed Miliband" height="349" width="366">',
 		];
 
-$('#pol_image').prepend(photo_array[photo_counter]);  
+/* '<img src="http://i.imgur.com/cmWb2x8.jpg" alt="David Cameron" height="349" width="366">',
+'<img src="http://i.imgur.com/15se11u.jpg" alt="Ed Miliband" height="349" width="366">', */
+
+$(function(){
+  $("#refresh_button").click(function(){
+  	photo_counter = photo_counter + 1;
+  	// var photo = photo_array[photo_counter];
+    $("#pol_image").prepend(photo_counter);
+  });
+  // prints an aphorism on mouse-click
+  $( "#refresh_button" ).trigger( "click" );
+
+});
+
+/* $(function(){
+	$("#refresh_button").click(function(){
+		var photo_counter = photo_counter + 1;
+		var photo = lemon_array[photo_counter];
+		$( "#pol_image" ).text(print_photo());
+	});
+	$( "#refresh_button" ).trigger( "click" );
+
+function print_photo() {
+	document.getElementById("pol_image").innerHTML = photo;
+    };
+
+}); */
 
 // on click, the 'photo credits' button slides out the details of any photo credits
 
